20944c55451fdef5fa6df943553d30ccb64288a0
nejishow <ashrafmuhamad2014@gmail.com>
2022-08-30 19:37:26 +0300

    changer le routing

--- src/components/other-pages/not-found/NotFound.vue
+++ src/components/other-pages/not-found/NotFound.vue
@@ -1,28 +1,31 @@
 <template>
-    <div>
-        <!-- Start 404 Error Area -->
-        <section class="error-area">
-            <div class="d-table">
-                <div class="d-table-cell">
-                    <div class="container">
-                        <div class="error-content">
-                            <div class="notfound-404">
-                                <h1>Oops!</h1>
-                            </div>
-                            <h3>404 - Page not found</h3>
-                            <p>The page you are looking for might have been removed had its name changed or is temporarily unavailable.</p>
-                            <a href="/" class="btn btn-primary">Go to Homepage</a>
-                        </div>
-                    </div>
-                </div>
+  <div>
+    <!-- Start 404 Error Area -->
+    <section class="error-area">
+      <div class="d-table">
+        <div class="d-table-cell">
+          <div class="container">
+            <div class="error-content">
+              <div class="notfound-404">
+                <h1>Oops!</h1>
+              </div>
+              <h3>404 - Page inexistante</h3>
+              <p>
+                La page que vous cherchez est soit inexistante soit
+                temporairement indisponible.
+              </p>
+              <a href="/" class="btn btn-primary">Aller à l'acceuil</a>
             </div>
-        </section>
-        <!-- End 404 Error Area -->
-    </div>
+          </div>
+        </div>
+      </div>
+    </section>
+    <!-- End 404 Error Area -->
+  </div>
 </template>
 
 <script>
 export default {
-    name: 'NotFound'
-}
+  name: "NotFound",
+};
 </script>
