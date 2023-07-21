e7c571b0e72000c0cfcae6462e531025f983b2fc
nejishow <ashrafmuhamad2014@gmail.com>
2023-07-21 20:58:03 +0300

    add eslint file

--- src/App.vue
+++ src/App.vue
@@ -1,74 +1,76 @@
 <template>
   <div>
-    <HeaderTwo v-if="currentUrl == '/web-hosting'"></HeaderTwo>
-    <HeaderThree v-else-if="currentUrl == '/machine-learning'"></HeaderThree>
-    <HeaderFour v-else-if="currentUrl == '/digital-agency'"></HeaderFour>
+    <HeaderTwo v-if="currentUrl == '/web-hosting'" />
+    <HeaderThree v-else-if="currentUrl == '/machine-learning'" />
+    <HeaderFour v-else-if="currentUrl == '/digital-agency'" />
     <div
       v-else-if="
         currentUrl == '/not-found' || currentUrl == '/coming-soon'
-      "></div>
-    <Header v-else></Header>
+      "
+    />
+    <Header v-else />
     <PreLoader v-if="isLoading" />
-    <router-view></router-view>
+    <router-view />
     <div
-      v-if="currentUrl == '/not-found' || currentUrl == '/coming-soon'"></div>
-    <Footer v-else></Footer>
+      v-if="currentUrl == '/not-found' || currentUrl == '/coming-soon'"
+    />
+    <Footer v-else />
   </div>
 </template>
 
 <script>
-import Header from "./components/layout/Header";
-import HeaderTwo from "./components/layout/HeaderTwo";
-import HeaderThree from "./components/layout/HeaderThree";
-import HeaderFour from "./components/layout/HeaderFour";
-import Footer from "./components/layout/Footer";
-import PreLoader from "./components/layout/PreLoader";
-import auth from "./services/auth";
+import Header from './components/layout/Header'
+import HeaderTwo from './components/layout/HeaderTwo'
+import HeaderThree from './components/layout/HeaderThree'
+import HeaderFour from './components/layout/HeaderFour'
+import Footer from './components/layout/Footer'
+import PreLoader from './components/layout/PreLoader'
+import auth from './services/auth'
 export default {
-  name: "app",
+  name: 'App',
   components: {
     Header,
     HeaderTwo,
     HeaderThree,
     HeaderFour,
     Footer,
-    PreLoader,
+    PreLoader
   },
 
-  data() {
+  data () {
     return {
       isLoading: true,
-      currentUrl: "",
-      emailVerified: false,
-    };
+      currentUrl: '',
+      emailVerified: false
+    }
   },
 
   watch: {
-    $route(pathUrl) {
-      this.currentUrl = pathUrl.path;
-      this.isLoading = true;
+    $route (pathUrl) {
+      this.currentUrl = pathUrl.path
+      this.isLoading = true
       setTimeout(() => {
-        this.isLoading = false;
-      }, 1500);
-    },
-  },
-  async beforeDestroy() {
-    await this.$store.dispatch("logout");
-  },
-  async mounted() {
-    const id = localStorage.getItem("idUser");
-    if (id != null || undefined) {
-      await this.$store.dispatch("afterLogin", id);
-      await this.$store.dispatch("setCoursesDisponibles");
-      await this.$store.dispatch("setMyCourses");
-    } else {
-      await auth.anonymous();
-      await this.$store.dispatch("setCoursesDisponibles");
+        this.isLoading = false
+      }, 1500)
     }
-    this.currentUrl = window.location.pathname;
-    setTimeout(() => {
-      this.isLoading = false;
-    }, 2000);
   },
-};
+  async beforeUnmount () {
+    await this.$store.dispatch('logout')
+  },
+  async mounted () {
+    const id = localStorage.getItem('idUser')
+    if (id != null || undefined) {
+      await this.$store.dispatch('afterLogin', id)
+      await this.$store.dispatch('setCoursesDisponibles')
+      await this.$store.dispatch('setMyCourses')
+    } else {
+      await auth.anonymous()
+      await this.$store.dispatch('setCoursesDisponibles')
+    }
+    this.currentUrl = window.location.pathname
+    setTimeout(() => {
+      this.isLoading = false
+    }, 2000)
+  }
+}
 </script>
