2c2e10292b661c679f7d0f94ae0017d1befc1030
nejishow <ashrafmuhamad2014@gmail.com>
2023-07-21 20:55:44 +0300

    update main.js

--- src/main.js
+++ src/main.js
@@ -1,38 +1,38 @@
-import Vue from 'vue'
-import VueRouter from 'vue-router';
-import { BootstrapVue } from 'bootstrap-vue';
-import VueFeather from 'vue-feather';
-import vWow from 'v-wow';
-import VueCarousel from 'vue-carousel';
-import Toasted from 'vue-toasted';
-import App from './App.vue'
-import './assets/style/custom.scss';
-import { router } from './router';
-import store from './store';
+/**
+ * main.js
+ *
+ * Bootstraps Vuetify and other plugins then mounts the App`
+ */
 
-Vue.config.productionTip = false;
+// Components
+import App from './App.vue'
+
+// Composables
+import { createApp } from 'vue'
+
+import VueFeather from 'vue-feather'
+import vWow from 'v-wow'
+import VueCarousel from 'vue-carousel'
+import './assets/style/custom.scss'
+import router from './router'
+import store from './store/index'
 
 import { fb, db } from '../db.js'
+// Plugins
+const app = createApp(App)
+app.use(router)
+app.use(store)
 
-Vue.use(fb)
-Vue.use(db)
+app.config.productionTip = false
+
+app.use(fb)
+app.use(db)
 
 // if using Firebase JS SDK < 5.8.0
 db.settings({ timestampsInSnapshots: true })
 
+app.use(VueFeather)
+app.use(vWow)
+app.use(VueCarousel)
 
-
-Vue.use(BootstrapVue);
-Vue.use(VueFeather);
-Vue.use(VueRouter);
-Vue.use(vWow);
-Vue.use(VueCarousel);
-
-Vue.use(Toasted, {
-  iconPack: 'fontawesome'
-});
-new Vue({
-  store,
-  router,
-  render: h => h(App),
-}).$mount('#app')
+router.isReady().then(() => app.mount('#app'))
